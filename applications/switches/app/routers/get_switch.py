65311f261818e63d10958e7dcbf21dbe7405faea
Vinayak Saxena <saxenavinayak12@gmail.com>
2026-03-08 22:43:01 -0400

    toggle endpoint

--- applications/switches/app/routers/get_switch.py
+++ applications/switches/app/routers/get_switch.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from kasa import Discover, Credentials
+from pydantic import BaseModel
 from ..deps import require_env_vars
 from ..arguments import host_map, KASA_PASSWORD, KASA_USERNAME
 
@@ -8,6 +9,10 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
     dependencies=[Depends(require_env_vars)]
 )
+
+class switch_state(BaseModel):
+    switch_id: str
+    is_on: bool
 
 @router.get("/")
 async def get_lights():
@@ -30,3 +35,20 @@ async def get_switch_status(switch_id: str):
         "location": dev.location,
         "mac_addy": dev.mac
     }
+
+@router.put("/toggle")
+async def toggle_switch(desired_state: switch_state):
+    if (desired_state.switch_id not in host_map):
+        return {"Error": f"Switch ID must be one of: {list(host_map.keys())}"}
+    creds = Credentials(username=KASA_USERNAME, password=KASA_PASSWORD)
+    dev = await Discover.discover_single(credentials=creds, host=host_map[desired_state.switch_id])
+
+    await dev.update()
+    if desired_state.is_on:
+        await dev.turn_on()
+    else:
+        await dev.turn_off()
+    return {f"{desired_state.switch_id} ON status": desired_state.is_on}
+
+
+
